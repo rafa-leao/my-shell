bcc404d9e9ae93c343d27e452455282c1b7bc5f9
Rafael Leão <rleao53@gmail.com>
2020-01-02 18:29:22 -0300

    Add docker-composer installer

--- mongodb.sh
+++ mongodb.sh
@@ -1,4 +1,4 @@
-# I saw it here: https://docs.mongodb.com/manual/tutorial/install-mongodb-on-ubuntu/
+# reference: https://docs.mongodb.com/manual/tutorial/install-mongodb-on-ubuntu/
 
 wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
 
