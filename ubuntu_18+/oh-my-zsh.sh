526f2fa617f411927f84770bb8f73929de525523
Gabriel Vacari <gabriel_vacari@outlook.com>
2020-02-07 08:27:50 -0300

    installing curl and setting zsh default

--- ubuntu_18+/oh-my-zsh.sh
+++ ubuntu_18+/oh-my-zsh.sh
@@ -1,9 +1,17 @@
 sudo apt update
 
-echo "Installing zsh first"
+echo "Installing curl first"
+
+sudo apt install curl
+
+echo "Installing zsh"
 
 sudo apt install zsh
 
 echo "Installing oh-my-zsh with curl"
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+echo "Setting zsh shell default"
+
+chsh -s $(which zsh)
