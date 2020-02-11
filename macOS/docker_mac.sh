49bbe6b58af490e221f2c248c0e4615fca137d55
Gabriel Vacari <gabriel_vacari@outlook.com>
2020-02-11 10:15:55 -0300

    fixing error

--- macOS/docker_mac.sh
+++ macOS/docker_mac.sh
@@ -9,10 +9,15 @@ brew install docker-machine
 # Installing virtualBox becausa docker cant create machine without vb
 brew cask install virtualbox
 
-echo "If you see an error message follow this tutorial https://medium.com/@yutafujii_59175/a-complete-one-by-one-guide-to-install-docker-on-your-mac-os-using-homebrew-e818eb4cfc3"
-echo "Successfully installed? Nice! run this shell again"
+echo "Any error message? [y/N]"
+read error
 
-exit 1
+if [ $error ]; then
+    echo "If you see an error message follow this tutorial https://medium.com/@yutafujii_59175/a-complete-one-by-one-guide-to-install-docker-on-your-mac-os-using-homebrew-e818eb4cfc3"
+    echo "Successfully installed? Nice! run this shell again"
+    exit 1
+fi
+
 
 # Creating a machine
 docker-machine create --driver virtualbox default
